42d3c2121ff444446cccd8a5036b496004b1b9b6
Jonathan Christ <matthewc.calaycay@gmail.com>
2023-12-30 12:21:34 +0800

    fixed background again

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import bgImage from './assets/bg.png'
 import './App.css'
 
 function App() {
@@ -11,6 +12,9 @@ function App() {
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="#" target="_blank">
+          <img src={bgImage} className="logo w-full h-full" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
